720e9b5d688ab7d519345279189c9e8186ffdf0f
Alexei Guevara <alexguev@gmail.com>
2013-03-09 10:45:33 -0500

    fully test the successor function

--- test/solve_test.clj
+++ test/solve_test.clj
@@ -24,16 +24,89 @@
                         4  5  6  7
                         8  9 10 11
                        12 13 14 15])
-    (def state-left [1  2  0  3
-                     4  5  6  7
-                     8  9 10 11
-                    12 13 14 15])
     (def state-down [1  2  3  7
                      4  5  6  0
                      8  9 10 11
                     12 13 14 15])
+    (def state-left [1  2  0  3
+                     4  5  6  7
+                     8  9 10 11
+                    12 13 14 15])
     (is (= [{:path [] :cost 0 :state state-initial :action :down :next-state state-down}
             {:path [] :cost 0 :state state-initial :action :left :next-state state-left}]
-           (successor {state-initial {:cost 0 :path []}})))))
+           (successor {state-initial {:cost 0 :path []}}))))
+  (testing "bottom left edge"
+    (def state-initial [1  2  3  4
+                        5  6  7  8
+                        9 10 11 12
+                        0 13 14 15])
+    (def state-up [1  2  3  4
+                   5  6  7  8
+                   0 10 11 12
+                   9 13 14 15])
+    (def state-right [1  2  3  4
+                      5  6  7  8
+                      9 10 11 12
+                     13  0 14 15])
+    (is (= [{:path [] :cost 0 :state state-initial :action :up :next-state state-up}
+            {:path [] :cost 0 :state state-initial :action :right :next-state state-right}]
+           (successor {state-initial {:cost 0 :path []}}))))
+  (testing "bottom right edge"
+    (def state-initial [1  2  3  4
+                        5  6  7  8
+                        9 10 11 12
+                       13 14 15  0])
+    (def state-up [1  2  3  4
+                   5  6  7  8
+                   9 10 11  0
+                  13 14 15 12])
+    (def state-left [1  2  3  4
+                     5  6  7  8
+                     9 10 11 12
+                    13 14  0 15])
+    (is (= [{:path [] :cost 0 :state state-initial :action :up :next-state state-up}
+            {:path [] :cost 0 :state state-initial :action :left :next-state state-left}]
+           (successor {state-initial {:cost 0 :path []}}))))
+  (testing "center"
+    (def state-initial [1  2  3  4
+                        5  0  7  8
+                        9 10 11 12
+                       13 14 15  6])
+    (def state-up [1  0  3  4
+                   5  2  7  8
+                   9 10 11 12
+                  13 14 15  6])
+    (def state-right [1  2  3  4
+                      5  7  0  8
+                      9 10 11 12
+                     13 14 15  6])
+    (def state-down [1  2  3  4
+                     5 10  7  8
+                     9  0 11 12
+                    13 14 15  6])
+    (def state-left [1  2  3  4
+                     0  5  7  8
+                     9 10 11 12
+                    13 14 15  6])
+    (is (= [{:path [] :cost 0 :state state-initial :action :up :next-state state-up}
+            {:path [] :cost 0 :state state-initial :action :right :next-state state-right}
+            {:path [] :cost 0 :state state-initial :action :down :next-state state-down}
+            {:path [] :cost 0 :state state-initial :action :left :next-state state-left}]
+           (successor {state-initial {:cost 0 :path []}}))))  
+  (testing "exclude state already in path"
+    (def state-initial [1  2  3  4
+                        5  6  7  8
+                        9 10 11 12
+                       13 14 15  0])
+    (def state-up [1  2  3  4
+                   5  6  7  8
+                   9 10 11  0
+                  13 14 15 12])
+    (def state-left [1  2  3  4
+                     5  6  7  8
+                     9 10 11 12
+                    13 14  0 15])
+    (is (= [{:path [state-up :down] :cost 1 :state state-initial :action :left :next-state state-left}]
+           (successor {state-initial {:cost 1 :path [state-up :down]}})))))
 
 (run-tests 'solve-test)
